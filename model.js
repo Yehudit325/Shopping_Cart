c0f3a57f23c67945e92b58306d4e31bedef70d59
Yehudit325 <yehudithalb@gmail.com>
2019-06-16 11:25:27 +0300

    feat: started project: add images to project and start array of all products info

---
+++ model.js
@@ -0,0 +1,35 @@
+
+let products = [
+    { id: 1, name: "Bamba", image: ".\images\bamba.png", price: 4.3, catagory: "Dry" },
+    { id: 2, name: "Banana", image: ".\images\banana.jpg", price: 5.9, catagory: "Fruits & Vegetables" },
+    { id: 3, name: "Bread", image: ".\images\bread.jpg" , price: 6.1, catagory: "Dry" },
+    { id: 4, name: "Carrots", image: ".\images\carrots.jpg" , price: 3.9, catagory: "Fruits & Vegetables" },
+    { id: 5, name: "Cheese", image: ".\images\cheese.jpg" , price: 7.6, catagory: "Milk Products" }, 
+    { id: 6, name: "Chicken", image: ".\images\chicken.jpg", price: 27.9, catagory: "Meat" },
+    { id: 7, name: "Crackers", image: ".\images\crackers.png" , price: 7.5, catagory: "Dry" },
+    { id: 8, name: "Cucumber", image: ".\images\cucumber.jpg" , price: 4.9, catagory: "Fruits & Vegetables" },
+    { id: 9, name: "Eggs", image: ".\images\eggs.png" , price: 11.3, catagory: "Milk Products"  },
+    { id: 10, name: "Ice Cream", image: ".\images\ice-cream.png" , price: 17.9, catagory: "Milk Products" },
+    { id: 11, name: "Milk", image: ".\images\milk.jpg" , price: 5.9, catagory: "Milk Products" },
+    { id: 12, name: "Onion", image: ".\images\onion.jpg" , price: 3.9, catagory: "Fruits & Vegetables" },
+    { id: 13, name: "Orange", image: ".\images\orange.jpg" , price: 6.9, catagory: "Fruits & Vegetables" },
+    { id: 14, name: "Pasta", image: ".\images\pasta.png" , price: 5.9, catagory: "Dry" },
+    { id: 15, name: "Potato", image: ".\images\potato.jpg" , price: 4.9, catagory: "Fruits & Vegetables" },
+    { id: 16, name: "Red Apple", image: ".\images\red-apple.jpg" , price: 9.9, catagory: "Fruits & Vegetables" },
+    { id: 17, name: "Red Pepper", image: ".\images\red-pepper.png" , price: 11.9, catagory: "Fruits & Vegetables"  },
+    { id: 18, name: "Rice", image: ".\images\rice.jpg" , price: 9.9 , catagory: "Dry" },
+    { id: 19, name: "Shnitzel", image: ".\images\shnitzel.jpg" , price: 35.9, catagory: "Meat" },
+    { id: 20, name: "Tomato", image: ".\images\tomato.png" , price: 4.9, catagory: "Fruits & Vegetables" },
+    { id: 21, name: "Water", image: ".\images\water.jpg" , price: 13.9, catagory: "Drinks" },
+    { id: 22, name: "Yogurt", image: ".\images\yogurt.png" , price: 3.2, catagory: "Milk Products" }
+  ];
+
+  let cart = []
+
+  function addProducttoInventory(name, img, price) {
+
+  }
+
+  function addProducttoCart() {
+      
+  }
