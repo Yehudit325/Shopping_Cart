5432504f18324a8b1619fc0388e3067d74aeedb8
Yehudit325 <yehudithalb@gmail.com>
2019-06-17 15:50:07 +0300

    feat: add item to cart + update amount

--- control.js
+++ control.js
@@ -11,9 +11,36 @@ class Controller {
         return productsData;
     }
 
-    addProductToCart(item) {
-        cart.push(item);
+    getCart() {
+        return cart;
     }
+
+    getItemFromInventory(item) {
+        return productsData[productsData.indexOf(item)];
+    }
+
+    getItemFromCart(item) {
+        return cart[cart.indexOf(item)];
+    }
+
+    addProductToCart(item) {
+        if (!cart.includes(item) && item.amount > 0) {
+            cart.push(item);
+        }
+    }
+
+    updateAmount(item, value) {
+        productsData[productsData.indexOf(item)].amount = value;
+        if (cart.includes(item)) {
+            cart[cart.indexOf(item)].amount = value;
+        }
+    }
+
+    updatePrice() {
+        
+    }
+
+   
 }
 
 const shoppingCartApp = new Controller(productsView);
--- view.js
+++ view.js
@@ -1,10 +1,10 @@
 class View {
 
 	init() {
-        this.renderProducts();
+        this.renderInventory();
     }
 
-    renderProducts() {
+    renderInventory() {
         const products = shoppingCartApp.getProducts();
         const inventory = document.getElementById('inventory');
         inventory.innerHTML = ''; // clear HTML from the DOM
@@ -38,13 +38,14 @@ class View {
             input.type = "number";
             input.className = "quantity";
             input.min = "0";
-            input.value = "0";
+            input.value = products[i].amount;
+            input.addEventListener('input', () => this.updateAmount(products[i], input.value));
 
             // Create button element
             let button = document.createElement("BUTTON");
             button.innerText = "Add to cart";
             button.className = "add-item";
-            button.addEventListener('click', this.addToCart);
+            button.addEventListener('click', () => this.addToCart(products[i]));
 
             // Append child nodes accordigly      
             itemInfo.append(p1, p2, input, button);
@@ -53,10 +54,92 @@ class View {
         }
     }
 
-    addToCart() {
-        shoppingCartApp.addProductToCart(item);
+    updateAmount(item, value) {
+        shoppingCartApp.updateAmount(item, value);
+        // let cartItem = shoppingCartApp.getItemFromCart(item);
+        // let inventoryItem = shoppingCartApp.getItemFromInventory(item);
+        // console.log(cartItem, inventoryItem);
+        // if (cartItem)
+            // document.getElementsByClassName("item-amount")[itemIndex].value = cartItem.amount;
+        // document.getElementsByClassName("quantity")[0].value = inventoryItem.amount;
+        this.renderCart(); // IMRPOVE: only rerender inoput value without looping through entire array 
+        this.renderInventory(); // IMRPOVE: only rerender inoput value without looping through entire array
     }
+
+    addToCart(item) {
+        shoppingCartApp.addProductToCart(item);
+        this.renderCart();
+    }
+
+    renderCart() {
+        const cartProducts = shoppingCartApp.getCart();
+        const cart = document.getElementById('cart');
+        cart.innerHTML = ''; // clear HTML from the DOM
+        for (let i = 0; i < cartProducts.length; ++i) {
+            // Create outer divs
+            let item = document.createElement('div');
+            item.className = "buy-item";
+            let itemPic = document.createElement('div');
+            itemPic.className = "item-pic2";
+
+            // Create image element
+            let pic = document.createElement('img');
+            pic.src = cartProducts[i].image;
+            pic.alt = cartProducts[i].name;
+            itemPic.append(pic);
+
+            // Create product info div
+            let itemInfo = document.createElement('div');
+            itemInfo.className = "item-info2";
+
+            // Create text elements
+            let p1 = document.createElement("P"); 
+            p1.innerText = cartProducts[i].name;
+            p1.className = "product-name";
+            let p2 = document.createElement("P"); 
+            p2.innerText = cartProducts[i].price + "₪";  // add per kilo to price for friuts and vegis
+            p2.className = "price";
+
+            // Create input element
+            let input = document.createElement("input");
+            input.type = "number";
+            input.className = "item-amount";
+            input.min = "0";
+            input.value = cartProducts[i].amount;
+            input.addEventListener('input', () => this.updateAmount(cartProducts[i], input.value));
+
+            // Create button element
+            let button = document.createElement("I");
+            button.innerText = "cancel";
+            button.className = "material-icons cancel-btn";
+            button.addEventListener('click', this.cancelProduct);
+
+            // Append child nodes accordigly      
+            itemInfo.append(p1, input, p2, button);
+            item.append(itemPic, itemInfo);
+            cart.append(item);
+        }
+    }
+    
+    cancelProduct() {
+        console.log("product canceled");
+    }
+
+
 }
+
+{/* <div id="cart" class="shopping-cart">
+            <div class="buy-item">
+                <div class="item-pic2">
+                    <img src="images\orange.jpg">
+                </div>
+                <div class="item-info2">
+                    <p>name</p>
+                    <input class="item-amount" type="number" min="0">
+                    <p id="total-price">price</p>
+                    <i class="material-icons cancel-btn">cancel</i>
+                </div>
+            </div> */}
 
 
 const productsView = new View();
